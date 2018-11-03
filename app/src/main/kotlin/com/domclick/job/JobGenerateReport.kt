bca18e3e24e311bebf9c626a78fdcf327996e3ad
ivan9105 <ivan9105@bk.ru>
2018-11-03 10:28:33 +0400

    quartz #2

--- app/src/main/kotlin/com/domclick/job/JobGenerateReport.kt
+++ app/src/main/kotlin/com/domclick/job/JobGenerateReport.kt
@@ -1,5 +1,15 @@
 package com.domclick.job
 
-class JobGenerateReport {
-    //Todo
+import com.domclick.service.GenerateReportService
+import org.quartz.DisallowConcurrentExecution
+import org.quartz.Job
+import org.quartz.JobExecutionContext
+
+@DisallowConcurrentExecution
+class JobGenerateReport(private val service: GenerateReportService) : Job {
+
+
+    override fun execute(context: JobExecutionContext?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 }
